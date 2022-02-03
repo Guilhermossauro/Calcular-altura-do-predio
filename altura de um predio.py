6a45eccf019b9aaa50866827246e54a951bdfcc7
Guilhermossauro <56206350+Guilhermossauro@users.noreply.github.com>
2022-02-03 17:18:39 -0300

    Add files via upload

---
+++ altura de um predio.py
@@ -0,0 +1,13 @@
+# Calculador de altura
+
+numero1 = 0
+numero2 = 0
+total = 0
+   
+numero1 = int(input("digite a quantidade de andares"))
+numero2 = int(input("digite a altura de cada andar"))
+
+total = numero1*numero2
+
+print ("\na altura total do prédio é de",total,'metros')
+input("\nPressione qualquer tecla para finalizar")
